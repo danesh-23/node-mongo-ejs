2e0a3da735b972fb0837dca0ece7f09180e204d2
Danesh Rajasolan <daneshpbc@gmail.com>
2024-05-22 01:34:10 +1000

    add search and add new author to database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
@@ -13,8 +14,11 @@ app.set('layout', 'layouts/layout') // prevent duplication of beginning and endi
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
